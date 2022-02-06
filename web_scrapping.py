317846d6432d5e7bf0ffe2f946b38aec025d4e90
pauloestrellalabs <paulo.netto@luizalabs.com>
2022-02-06 16:37:36 -0300

    Add type hits and change bs4 how  it was called

--- web_scrapping.py
+++ web_scrapping.py
@@ -1,12 +1,11 @@
-import csv, time, re, requests
-from bs4 import BeautifulSoup
+import csv, bs4,  time, re, requests
 
 url = "https://www12.senado.leg.br/ecidadania"
 filter = "+".join(input("Digite o filtro de busca: ").lower().split())
 first_page = requests.get(f"{url}/pesquisaideia?pesquisa={filter}", headers={'Cache-Control': 'no-cache'})
 
-def find_last_page_to_search(first_page):
-    parser = BeautifulSoup(first_page.content, 'html.parser')
+def find_last_page_to_search(first_page: requests.models.Response) -> int:
+    parser = bs4.BeautifulSoup(first_page.content, 'html.parser')
     pagination = parser.find_all("a", {"class": "page-link", "title": "último"})
     if len(pagination) > 0:
         last_page = int(re.search("p=[0-9]{1,4}", str(pagination)).group(0).replace("p=", ""))
@@ -20,12 +19,12 @@ start_time = time.time()
 for page in range(1, last_page+1):
     print(f"Página {page} sendo executada")
     data = requests.get(f"{url}/pesquisaideia?p={page}&pesquisa={filter}", headers={'Cache-Control': 'no-cache'})
-    data_parser = BeautifulSoup(data.content, 'html.parser')
+    data_parser = bs4.BeautifulSoup(data.content, 'html.parser')
     proposes = data_parser.find_all("article", {"class":"resumo-ideia"})
     for idea in proposes:
         id = idea.find("a")["href"]
         data_idea = requests.get(f"{url}/{id}")
-        article_parser = BeautifulSoup(data_idea.content, 'html.parser')
+        article_parser = bs4.BeautifulSoup(data_idea.content, 'html.parser')
         title = article_parser.find_all("div", {"style":"font-size:24px;margin-bottom:15px;"})[0].text
         first_paragraph = article_parser.find_all("div", {"style": "margin-bottom:15px;"})[0].text
         second_paragraph = article_parser.find_all("div", {"id": "collapseOne"})[0].text
