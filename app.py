3799a8e25907e617c5eee46afc2dd0746b50bbc6
pauloestrellalabs <paulo.netto@luizalabs.com>
2022-02-13 16:56:28 -0300

    Run home page of flask application

---
+++ app.py
@@ -0,0 +1,12 @@
+from crypt import methods
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+@app.route("/home", methods=["GET",])
+def home_page():
+    return render_template('home_page.html')
+
+
+app.run(debug=True)
