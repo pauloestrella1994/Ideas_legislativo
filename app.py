e7a96e65cbde9ac079f23a0b03867e7019f84a1d
pauloestrellalabs <paulo.netto@luizalabs.com>
2022-02-13 18:21:29 -0300

    Add method post to home_page

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 
 app = Flask(__name__)
 
 @app.route("/home", methods=["GET","POST"])
 def home_page():
+    if request.method == "POST":
+        return redirect(url_for('processing'))
     return render_template('home_page.html')
+
+@app.route("/processing", methods=["GET"])
+def processing():
+    return render_template("processing.html")
 
 
 app.run(debug=True)
