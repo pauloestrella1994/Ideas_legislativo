78897197d5ac8f00da9a6fe8e2f1b4a55f8e93ad
pauloestrellalabs <paulo.netto@luizalabs.com>
2022-02-13 19:08:01 -0300

    Pass filter to redirect in processing template

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
+from distutils.command.build_scripts import first_line_re
 from flask import Flask, render_template, request, redirect, url_for
+from web_scrapping import ScrapeData
 
 
 app = Flask(__name__)
@@ -6,11 +8,13 @@ app = Flask(__name__)
 @app.route("/home", methods=["GET","POST"])
 def home_page():
     if request.method == "POST":
-        return redirect(url_for('processing'))
+        data = request.form.get("filter")
+        breakpoint()
+        return redirect(url_for('processing', data=data))
     return render_template('home_page.html')
 
-@app.route("/processing", methods=["GET"])
-def processing():
+@app.route("/processing/<data>", methods=["GET"])
+def processing(data):
     return render_template("processing.html")
 
 
